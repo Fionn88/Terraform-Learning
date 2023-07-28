c13a1e861e22fe09894ebbedc5d5888c72666cf3
FionnKuo <>
2023-07-29 02:25:37 +0800

    output variables in terraform way

---
+++ Terraform_for_the_Absolute_Beginners_with_Labs/output_variables/variables.tf
@@ -0,0 +1,15 @@
+variable filename {
+  default = "files/pets.txt"
+}
+
+variable prefix {
+  default     = "Mrs"
+}
+
+variable separator {
+  default     = "."
+}
+
+variable length {
+  default     = "1"
+}
