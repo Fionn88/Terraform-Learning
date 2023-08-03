d3c742dc614dfd2f39e09f3444732a177420dbbe
Fionn88 <coco8802231225@gmail.com>
2023-08-03 13:46:30 +0800

    count attribute in terraform way

---
+++ Terraform_for_the_Absolute_Beginners_with_Labs/count/variables.tf
@@ -0,0 +1,19 @@
+variable filename {
+    default = [
+        "files/pets.txt",
+        "files/dogs.txt",
+        "files/catws.txt",
+        "files/cows.txt",
+        "files/ducks.txt"
+    ]
+}
+
+variable content {
+    default = [
+        "We love pets!",
+        "We love dogs!",
+        "We love cats!",
+        "We love cows!",
+        "We love ducks!"
+    ]
+}
